5d93b34c11274071f0e9139f359431d44b6a591c
Moumita Das <dasmoumita.me@gmail.com>
2023-01-26 18:21:06 +0530

    user schema added

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,4 +1,7 @@
 const mongoose=requere("mongoose")
+const crypto =requere('node:crypto');
+import { v4 as uuidv4 } from 'uuid';
+
 
 const userSchema=new mongoose.Schema({
     name:{
@@ -18,9 +21,9 @@ const userSchema=new mongoose.Schema({
         unique:true,
         trim:true
     },
-    password:{
+    encry_password:{
         type:String,
-        trim:true
+        required:true
     },
     userinfo:{
         type:String,
@@ -37,5 +40,32 @@ const userSchema=new mongoose.Schema({
     }
 
 });
+
+userSchema.virtual("password")
+    .set(function(password){
+        this._password=password
+        this.salt=uuidv4();
+        this.encry_password=this.securePassword
+    })
+    .get(function(){
+        return this._password
+    })
+
+userSchema.method={
+    autheticate:function(plainPassword){
+        return this.securePassword(plainPassword)===this.encry_password
+    },
+    securePassword:function(plainPassword){
+        if(!plainPassword) return "";
+        try{
+            return crypto
+                .createHmac("sha256",this.salt)
+                .update(plainPassword)
+                .digest("hex");
+        }catch(err){
+            return ""
+        }
+    }
+}
 
 module.exports=mongoose.model("User",userSchema)
